9af3ea23babc229a943573daf2e0921a97b41d8d
Elliot Forsberg <elfo1106@storsjogymnasiet.se>
2023-04-03 13:05:42 +0200

    andra personbil

---
+++ personbil.py
@@ -0,0 +1,18 @@
+#klass som ärver fordon
+
+class Personbil:
+
+    #constructor
+    def __init__(self, fabrikat, color, bagagevolym):
+        self.fabrikat = fabrikat
+        self.color = color
+        self.bagagevolym = bagagevolym
+
+    def set_bagagevolym(self, bagagevolym):
+        self.bagagevolym = bagagevolym
+
+    def get_bagagevolym(self):
+        return self.bagagevolym
+    
+    def print_fordon(self):
+        print(self.fabrikat + "Färg: " + self.color + "Bagagevolym" + self.bagagevolym)
